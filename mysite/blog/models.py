47ed1420df070a99e7f4df049f3b0d9b2d06e5d4
Ankevi <maciekkubicki@gmail.com>
2020-08-11 16:45:43 +0200

    skończednie rozdziału 2

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 # Create your models here.
+
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -32,6 +34,8 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default='draft')
 
+    tags = TaggableManager()
+
     def get_absolute_url(self):
         return reverse('blog:post_detail',
                        args=[self.publish.year,
@@ -46,4 +50,18 @@ class Post(models.Model):
         return self.title
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
 
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Komentarz dodany przez {} dla posta {} '.format(self.name, self.post)
